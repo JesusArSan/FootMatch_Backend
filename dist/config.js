6f3d6784c2223b99f4c9b9e573591e79adce4084
jesusarsan <jesus02@correo.ugr.es>
2024-07-02 23:26:01 +0200

    Funciona Backend

--- dist/config.js
+++ dist/config.js
@@ -15,5 +15,5 @@ var config = exports.config = {
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_DATABASE || "test"
+  database: process.env.DB_DATABASE || "footmatch"
 };
