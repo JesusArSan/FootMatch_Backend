ea0b57e7828eb557ec318f612bb54ccb14186112
jesusarsan <jesus02@correo.ugr.es>
2024-08-24 15:22:41 +0200

    Update Backend .env

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ dotenv();
 export const config = {
 	host: process.env.DB_HOST || "localhost",
 	user: process.env.DB_USER || "root",
-	password: process.env.DB_PASSWORD || "",
+	password: process.env.DB_PASSWORD || "root",
 	database: process.env.DB_DATABASE || "footmatch",
 	waitForConnections: true,
 	connectionLimit: 10,
